19f0b56a5b749551a40f6dbbd7ce2161c095bf35
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-11-10 22:30:44 -0600

    added prompts to js

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// var wordLength = prompt("Please enter a length for the password between 8 and 128 characters.")
+// confirm("Would you like to include lowercase?")
+// confirm("Would you like to include uppercase?")
+// confirm("Would you like to include numeric characters?")
+// confirm("Would you like to include special characters?")
+
