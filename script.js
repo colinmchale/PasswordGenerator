0501b42f472a6a1a42765fc9240182dc938a2a8e
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2021-11-11 20:09:21 -0600

    added arrays that passwords will be generated from

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let selectedArrays = [];
+let lowerAlphabet = "abcdefghijklmnopqrstuvwxyz".toLowerCase().split("");
+let upperAlphabet = "abcdefghijklmnopqrstuvwxyz".toUpperCase().split("");
+let number = "1234567890".split("");
+let specialCharacter = "!#$%&()*+,-./:;<=>?@[]^_`{|}~".split("")
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -10,12 +16,14 @@ function writePassword() {
   
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+// let selectedArrays = [...lowerAlphabet, ...upperAlphabet]
 // var wordLength = prompt("Please enter a length for the password between 8 and 128 characters.")
 // confirm("Would you like to include lowercase?")
 // confirm("Would you like to include uppercase?")
 // confirm("Would you like to include numeric characters?")
 // confirm("Would you like to include special characters?")
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
